5398820b786c109af40effb2e91c8c57f44910b2
Radek Adamus <adamus.r@gmail.com>
2016-11-14 16:16:24 +0100

    adding dependencies

--- actions/hello.js
+++ actions/hello.js
@@ -1,8 +1,8 @@
 var task = function(configuration){
     return function(request, callback){
-        var template = "helloParamterized";
+        var template = "helloParamterized.ejs";
         var aws = configuration.aws;
-        
+
         callback(null, {template: template, params:{info:"Hello World from code!"}});
     }
 }
