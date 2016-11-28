4ccf95df5e76404b087f1deaff1c16c41a9b07e0
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2016-11-28 18:45:13 +0100

    listing buckets added

--- actions/hello.js
+++ actions/hello.js
@@ -3,7 +3,12 @@ var task =    function(request, callback, configuration){
         var template = "helloParamterized.ejs";
         var AWS = configuration.aws;
         var S3 = new AWS.S3();
-        callback(null, {template: template, params:{info:"Hello World from code!"}});
+        S3.listBuckets(function(err, data) {
+  if (err) {console.log(err, err.stack); // an error occurred
+      callback(null, {template: template, params:{info:"An error occured!"}});}
+  else     {callback(null, {template: template, params:{info:"Hello World from code!", buckets: data.Buckets}});}
+});
+        
     }
 
 
